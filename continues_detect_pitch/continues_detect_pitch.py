1f0deba94d845f11b754aeec03a763e4ce192589
root <root@localhost.localdomain>
2019-12-13 21:06:17 -0800

    1

---
+++ continues_detect_pitch/continues_detect_pitch.py
@@ -0,0 +1,50 @@
+import parselmouth
+import sounddevice as sd
+import numpy as np
+from scipy.io.wavfile import write
+from multiprocessing import Process, Pipe
+from threading import Thread
+
+sd.default.samplerate = 48000
+sd.default.channels = 1
+
+def record(duration: int) -> None:
+    recording = sd.rec(duration * sd.default.samplerate)
+    sd.wait()
+    write("w.wav", sd.default.samplerate, recording)
+
+
+def recording_to_pitch() -> float:
+    sound = parselmouth.Sound("w.wav")
+    pitch = sound.to_pitch(pitch_ceiling=999999, pitch_floor=3)
+    pitch_vals = pitch.selected_array['frequency']
+    average = np.nanmean(pitch_vals)
+    return average
+
+def record_and_return_pitch(duration: int) -> float:
+    record(duration)
+    return recording_to_pitch()
+
+
+def _continues_detect(pipe: Pipe, duration: int) -> None:
+    while True:
+        pitch = record_and_return_pitch(duration)
+        pipe.send(pitch)
+
+def continues_detect() -> Pipe:
+    p_r, p_w = Pipe(duplex=False)
+    Thread(target=_continues_detect, args=(p_w, 1)).start()
+    return p_r
+
+
+def continues_detect_and_print():
+    p_r = continues_detect()
+    while True:
+        pitch = p_r.recv()
+        print(pitch)
+
+def main():
+    continues_detect_and_print()
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,5 @@
+the continues detect pitch thing sort of work
+
+if you just run it directly it will print the results to screen (^C to stop)
+
+if you want to use it as a library just import it and call the function "continues_detect" and it will return a multiprocessing.Pipe that you can read he values from
